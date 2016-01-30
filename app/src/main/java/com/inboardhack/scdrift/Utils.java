742cdf95808ae186192ee8a0825234343e3565e1
Benjamin Ran <benjaminran2@gmail.com>
2016-01-29 18:39:00 -0800

    added isSliding

--- app/src/main/java/com/inboardhack/scdrift/Utils.java
+++ app/src/main/java/com/inboardhack/scdrift/Utils.java
@@ -1,1 +1,10 @@
-public 
+public class Utils {
+
+    public static final double MINSLIDESTRENGTH = 0;
+
+    public static boolean isSliding(double acceleration[], double rotation[], double speed) {
+        double caaccel = speed * rotation[5];
+        double slideStrength = Math.abs(caaccel) - Math.abs(acceleration[1]);
+        return (slideStrength > MINSLIDESTRENGTH);
+    }
+} 
