ba4e61cfcf75a7d0130628ffa5a78396a4b4e373
Benjamin Ran <benjaminran2@gmail.com>
2016-01-29 18:09:30 -0800

    added slide class

---
+++ app/src/main/java/com/inboardhack/scdrift/Slide.java
@@ -0,0 +1,17 @@
+public class Slide {
+
+    private int score;
+    private static final double ANGLEGAIN = 1;
+    private static final double ACCELERATIONGAIN = 1;
+
+    public Slide () {
+        score = 0;
+    }
+    public int getScore() {
+        return score;
+    }
+    public int incrementScore(double velocity[], double direction[], double acceleration[]) {
+        score += (int) (ANGLEGAIN * Math.sqrt(Math.pow(velocity[1]*direction[2] - velocity[2]*direction[1], 2) + Math.pow(velocity[2]*direction[0] - velocity[0]*direction[2], 2) + Math.pow(velocity[0]*direction[1] - velocity[1]*direction[0], 2)) + ACCELERATIONGAIN * Math.sqrt(Math.pow(acceleration[0], 2) + Math.pow(acceleration[1], 2) + Math.pow(acceleration[2], 2)));
+        return score;
+    }
+}
